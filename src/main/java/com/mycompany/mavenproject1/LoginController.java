899a8b41bdcf7a6aa893bf6fd9dc4c94327dea65
Pedro <algarra.delgado.pedro@alumnat.copernic.cat>
2023-05-22 17:26:15 +0200

    añadidas variables para el registro y cambio de pantalla entre botón registro y pantallamain funcional

--- src/main/java/com/mycompany/mavenproject1/LoginController.java
+++ src/main/java/com/mycompany/mavenproject1/LoginController.java
@@ -4,8 +4,12 @@
  */
 package com.mycompany.mavenproject1;
 
+import javafx.scene.control.TextField;
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 /**
  *
@@ -14,7 +18,27 @@ import javafx.fxml.FXML;
 public class LoginController {
 
     @FXML
+    private TextField userTextField;
+
+    @FXML
+    private TextField mailTextField;
+
+    @FXML
     private void cambiarPantallaMain() throws IOException {
-        App.setRoot("pantallaMainController");
+        String usuario = userTextField.getText();
+        String correo = mailTextField.getText();
+        
+        System.out.println("el usuario y el correo son : " + usuario + correo);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("pantallaMain.fxml"));
+        Parent root = loader.load();
+        PantallaMainController controller = loader.getController();
+
+        // Realiza cualquier configuración adicional en el controlador PantallaMainController si es necesario
+        // Obtén la escena actual y cámbiala por la nueva escena con la pantalla "PantallaMainController"
+        Scene scene = userTextField.getScene();
+        scene.setRoot(root);
+
+//        App.setRoot("pantallaMain.fxml");
     }
 }
