3d97b1e74c6be1ab143a11b20dd72379fd73c1a9
martinez.cordoba.ricardo <martinez.cordoba.ricardo@alumnat.copernic.cat>
2023-05-23 17:32:36 +0200

    Movido InGameController

--- src/main/java/com/mycompany/mavenproject1/Presentacion/InGameController.java
+++ src/main/java/com/mycompany/mavenproject1/Presentacion/InGameController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package com.mycompany.mavenproject1;
+package com.mycompany.mavenproject1.Presentacion;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -56,7 +56,7 @@ public class InGameController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
     
     @FXML
